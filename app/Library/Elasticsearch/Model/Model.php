ea6361c6c01e54efd677d4f0d24107c791526206
Amirhossein Pooladvand <a.h.pooladvand@gmail.com>
2019-07-13 18:25:05 +0430

    Add php doc on model.

--- app/Library/Elasticsearch/Model/Model.php
+++ app/Library/Elasticsearch/Model/Model.php
@@ -16,26 +16,31 @@ abstract class Model
 
     protected $connection;
 
-    public function __call($method, $parameters)
-    {
-        return (new Elasticsearch(new static))->$method(...$parameters);
-    }
-
-    public static function __callStatic($method, $parameters)
-    {
-        return (new static)->$method(...$parameters);
-    }
-
+    /**
+     * Index getter.
+     *
+     * @return string
+     */
     public function getIndex(): string
     {
         return $this->index;
     }
 
+    /**
+     * Connection setter.
+     *
+     * @param  string|null  $connection
+     */
     private function setConnection(string $connection = null): void
     {
         $this->connection = $connection;
     }
 
+    /**
+     * Connection getter.
+     *
+     * @return string
+     */
     protected function getConnection(): string
     {
         if (null === $this->connection) {
@@ -47,11 +52,40 @@ abstract class Model
         return $this->connection;
     }
 
+    /**
+     * Determines host and port of elasticsearch.
+     *
+     * @return string
+     */
     public function getHost(): string
     {
         $host = config("database.connections.{$this->getConnection()}.host");
         $port = config("database.connections.{$this->getConnection()}.port");
 
         return $host.':'.$port;
+    }
+
+    /**
+     * Fires when calling method which doesnt exists.
+     *
+     * @param $method
+     * @param $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        return (new Elasticsearch(new static))->$method(...$parameters);
+    }
+
+    /**
+     * Fires when calling static method which doesnt exists.
+     *
+     * @param $method
+     * @param $parameters
+     * @return mixed
+     */
+    public static function __callStatic($method, $parameters)
+    {
+        return (new static)->$method(...$parameters);
     }
 }
